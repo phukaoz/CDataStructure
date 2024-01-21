64c9d0d4ceef1d764486f56d892fddb244e3a368
phukaoz <tnpkofficial@gmail.com>
2024-01-21 14:21:04 +0700

    change struct type

--- vector.h
+++ vector.h
@@ -14,16 +14,18 @@ typedef struct{
     size_t capacity;
     TYPE *array;
     size_t data_size;
-} vector;
+} vector_struct;
 
-void vector_init(vector *vector_name){
+typedef vector_struct vector[1];
+
+void vector_init(vector vector_name){
     vector_name->size = 0;
     vector_name->capacity = 4;
     vector_name->data_size = sizeof(TYPE);
     vector_name->array = (TYPE*)malloc(vector_name->capacity*vector_name->data_size);
 }
 
-void vector_push_back(vector *vector_name,TYPE value){
+void vector_push_back(vector vector_name,TYPE value){
     if(vector_name->size == vector_name->capacity){
         vector_name->capacity = (vector_name->capacity << 1);
         vector_name->array = (TYPE*)realloc(vector_name->array,vector_name->capacity*vector_name->data_size);
@@ -31,7 +33,7 @@ void vector_push_back(vector *vector_name,TYPE value){
     vector_name->array[vector_name->size++] = value;
 }
 
-void vector_pop_back(vector *vector_name){
+void vector_pop_back(vector vector_name){
     if(vector_name->size != 0){
         vector_name->size--;
     }else{
@@ -40,7 +42,7 @@ void vector_pop_back(vector *vector_name){
     }
 }
 
-TYPE vector_back(vector *vector_name){
+TYPE vector_back(vector vector_name){
     if(vector_name->size == 0){
         fprintf(stderr, "index out of bound.");
         exit(-1);
@@ -48,11 +50,11 @@ TYPE vector_back(vector *vector_name){
     return vector_name->array[vector_name->size-1];
 }
 
-bool vector_empty(vector * vector_name){
+bool vector_empty(vector vector_name){
     return (vector_name->size == 0 ? true : false);
 }
 
-TYPE vector_get(vector *vector_name,size_t index){
+TYPE vector_get(vector vector_name,size_t index){
     if(index >= vector_name->size){
         fprintf(stderr, "index out of bound.");
         exit(-1);
@@ -64,23 +66,23 @@ TYPE vector_get(vector *vector_name,size_t index){
     return vector_name->array[index];
 }
 
-TYPE* vector_begin(vector *vector_name){
+TYPE* vector_begin(vector vector_name){
     return &(vector_name->array[0]);
 }
 
-TYPE* vector_end(vector *vector_name){
+TYPE* vector_end(vector vector_name){
     return &(vector_name->array[vector_name->size]);
 }
 
 typedef struct{
-    void (*init)(vector*);
-    void (*push_back)(vector*,TYPE);
-    void (*pop_back)(vector*);
-    TYPE (*back)(vector*);
-    TYPE (*get)(vector*,size_t);
-    bool (*empty)(vector*);
-    TYPE* (*begin)(vector*);
-    TYPE* (*end)(vector*);
+    void (*init)(vector);
+    void (*push_back)(vector, TYPE);
+    void (*pop_back)(vector);
+    TYPE (*back)(vector);
+    TYPE (*get)(vector, size_t);
+    bool (*empty)(vector);
+    TYPE* (*begin)(vector);
+    TYPE* (*end)(vector);
 }vector_controller;
 
 vector_controller CONTROLLER;
